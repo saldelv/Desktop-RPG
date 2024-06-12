3aedddc4a79ce32e9309dcf3c686d7d42a73725e
Salvatore Del Vecchio <96501610+saldelv@users.noreply.github.com>
2024-06-12 14:13:41 -0400

    Added hover tooltip

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 import random
 import tkinter as tk
+from tkinter import Toplevel, Label, LEFT, SOLID
 import pyautogui
 
+# Tooltip hover class
+class Tooltip(object):
+
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text):
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() - 25
+        y = y + cy + self.widget.winfo_rooty() - 70
+        self.tipwindow = tw = Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        tw.wm_attributes('-topmost', '1')
+        tlabel = Label(tw, text=self.text, justify=LEFT, bg='white', relief=SOLID, borderwidth=1, font='Gill')
+        tlabel.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+    
+def CreateToolTip(widget, text):
+    tooltip = Tooltip(widget)
+    def enter(event):
+        tooltip.showtip(text)
+    def leave(event):
+        tooltip.hidetip()
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
+
+
+# Default variables
 x = 2200
 y = 1280
 cycle = 0
@@ -13,10 +54,13 @@ event_number = random.randrange(1, 2, 1)
 
 window = tk.Tk()
 
+# Set gifs
 idle = [tk.PhotoImage(file='stand.gif', format = 'gif -index %i' %(i)) for i in range(2)]
 walk_positive = [tk.PhotoImage(file='left.gif', format = 'gif -index %i' %(i)) for i in range(2)]
 walk_negative = [tk.PhotoImage(file='right.gif', format = 'gif -index %i' %(i)) for i in range(2)]
 
+
+# Play gif and potentially change event
 def gif_work(cycle, frames, event_number, first_num, last_num):
     if cycle < len(frames) - 1:
         cycle += 1
@@ -25,6 +69,7 @@ def gif_work(cycle, frames, event_number, first_num, last_num):
         event_number = random.randrange(first_num, last_num + 1, 1)
     return cycle, event_number
 
+# Update current event
 def update(cycle, check, event_number, x):
     if check == 0:
         frame = idle[cycle]
@@ -41,9 +86,10 @@ def update(cycle, check, event_number, x):
         x += 3
 
     window.geometry('100x100+' + str(x) + '+' + str(y))
-    label.configure(image = frame)
+    character.configure(image = frame)
     window.after(1, event, cycle, check, event_number, x)
 
+# Set event
 def event(cycle, check, event_number, x):
     if event_number in idle_num:
         check = 0
@@ -60,13 +106,17 @@ def event(cycle, check, event_number, x):
         print('walk right')
         window.after(100, update, cycle, check, event_number, x)
 
+# Make background transparent and always on top
 window.config(highlightbackground='black')
 window.overrideredirect(True)
 window.wm_attributes('-transparentcolor', 'black')
 window['bg'] = 'black'
+window.wm_attributes('-topmost', '1')
 
-label = tk.Label(window, bd=0, bg='black')
-label.pack()
+character = tk.Label(window, bd=0, bg='black')
+character.pack()
+
+CreateToolTip(character, text = "Health: 100/100 \nStrength: 1 \nDefense: 1")
 
 window.after(1, update, cycle, check, event_number, x)
 
