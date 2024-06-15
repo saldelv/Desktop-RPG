6e35aabd7e49fd9317dc8e14068d05a14939ecfe
Salvatore Del Vecchio <96501610+saldelv@users.noreply.github.com>
2024-06-14 21:26:10 -0400

    Added all inventory features

--- main.py
+++ main.py
@@ -39,18 +39,22 @@ class Tooltip(object):
     
 def CreateToolTip(widget, text):
     tooltip = Tooltip(widget)
-    def enter(event):
+    def enter(set_event):
         tooltip.showtip(text)
-    def leave(event):
+    def leave(set_event):
         tooltip.hidetip()
     widget.bind('<Enter>', enter)
     widget.bind('<Leave>', leave)
 
-def do_menu(event):
+def UnbindToolTip(widget):
+    widget.unbind('<Enter>')
+    widget.unbind('<Leave')
+
+def do_menu(event, menu):
     try:
-        m.tk_popup(event.x_root, event.y_root)
+        menu.tk_popup(event.x_root, event.y_root)
     finally:
-        m.grab_release()
+        menu.grab_release()
 
 def open_equipment():
     eroot = tk.Toplevel(root)
@@ -60,16 +64,12 @@ def open_equipment():
     eroot.geometry("+%d+%d" % (ex, ey))
     eroot.wm_attributes('-topmost', '1')
 
-    if equiped[0] != "":
-        sword_image = tk.PhotoImage(file = equiped[0] + '.png', master=eroot)
-    else:
-        sword_image = tk.PhotoImage(file = 'sword.png', master=eroot)
-    weapon = tk.Button(eroot, text = "Weapon", image = sword_image, command=lambda: equip(0, eroot))
-    weapon.image = sword_image
-    headgear = tk.Button(eroot, text="Headgear", command=lambda: equip(1, eroot))
-    chestpiece = tk.Button(eroot, text="Chest Piece", command=lambda: equip(2, eroot))
-    leggings = tk.Button(eroot, text="Leggings", command=lambda: equip(3, eroot))
-    boots = tk.Button(eroot, text="Boots", command=lambda: equip(4, eroot))
+    
+    weapon = get_equipped(0, eroot)
+    headgear = get_equipped(1, eroot)
+    chestpiece = get_equipped(2, eroot)
+    leggings = get_equipped(3, eroot)
+    boots = get_equipped(4, eroot)
 
     weapon.pack()
     headgear.pack()
@@ -87,7 +87,67 @@ def open_equipment():
     
     eroot.mainloop()
 
-def equip(slot, eroot):
+def get_equipped(slot, eroot):
+    match slot:
+        case 0:
+            func = get_weapon
+            empty = 'Empty Sword.png'
+            item = "Sword"
+        case 1:
+            func = get_helmet
+            empty = 'Empty Helmet.png'
+            item = "Helmet"
+        case 2:
+            func = get_chestpiece
+            empty = 'Empty Chestpiece.png'
+            item = "Chestpiece"
+        case 3:
+            func = get_leggings
+            empty = 'Empty Leggings.png'
+            item = "Leggings"
+        case 4:
+            func = get_boots
+            empty = 'Empty Boots.png'
+            item = "Boots"
+
+    button = tk.Button(eroot, command=lambda: open_inventory(slot, eroot))
+    if equiped[slot] != "":
+        img = tk.PhotoImage(file = 'assets/items/' + equiped[slot] + '.png', master=eroot)
+        rows = func(equiped[slot])
+        for row in rows:
+            CreateToolTip(button, text = str(row[0]) + "\nAttack: " + str(row[1]) + "\nValue: " + str(row[2]))
+
+        em = tk.Menu(root, tearoff = 0)
+        em.add_command(label="Unequip", command=lambda: remove_equip(slot, eroot))
+        em.add_command(label="Sell", command=lambda: sell_equip(slot, -1, [], eroot))
+        button.bind("<Button-3>", lambda event, menu = em: do_menu(event, menu))
+    else:
+        img = tk.PhotoImage(file = 'assets/items/' + empty, master=eroot)
+        CreateToolTip(button, text=item)
+    button.configure(image=img)
+    button.image = img
+    
+    return button
+
+
+def open_inventory(slot, eroot):
+    match slot:
+        case 0:
+            inv = weapon_inventory
+            func = get_weapon
+        case 1:
+            inv = helmet_inventory
+            func = get_helmet
+        case 2:
+            inv = chestpiece_inventory
+            func = get_chestpiece
+        case 3:
+            inv = leggings_inventory
+            func = get_leggings
+        case 4:
+            inv = boots_inventory
+            func = get_boots
+    
     iroot = tk.Toplevel(eroot)
     iroot.resizable(0, 0)
     ix = eroot.winfo_x() - 170
@@ -95,58 +155,198 @@ def equip(slot, eroot):
     iroot.geometry("+%d+%d" % (ix, iy))
     iroot.wm_attributes('-topmost', '1')
 
-    inv = 0
+    index = 0
     for i in range(5):
         for j in range(5):
             b = tk.Button(iroot)
-            if inv < len(weapon_inventory):
-                if weapon_inventory[inv]:
-                    img = tk.PhotoImage(file = weapon_inventory[inv] + ".png", master=iroot)
-                    current = inv
-                    b.configure(image=img, command=lambda: new_equip(slot, weapon_inventory[current], iroot, eroot))
+            if index < len(inv):
+                if inv[index]:
+                    img = tk.PhotoImage(file = "assets/items/" + inv[index] + ".png", master=iroot)
+                    current = index
+                    b.configure(image=img, command=lambda: new_equip(slot, current, iroot, eroot))
                     b.image = img
-                inv += 1
+
+                    rows = func(inv[index])
+                    for row in rows:
+                        CreateToolTip(b, text = str(row[0]) + "\nAttack: " + str(row[1]) + "\nValue: " + str(row[2]))
+                    
+                    im = tk.Menu(root, tearoff = 0)
+                    im.add_command(label="Sell", command=lambda: sell_equip(slot, current, inv, iroot))
+                    b.bind("<Button-3>", lambda event, menu = im: do_menu(event, menu))
+
+                index += 1
             else:
-                img = tk.PhotoImage(file = "empty.png", master=iroot)
+                img = tk.PhotoImage(file = "assets/items/empty.png", master=iroot)
                 b.configure(image=img)
                 b.image=img
+
             b.grid(row=i, column=j)
 
-def new_equip(slot, new_weapon, iroot, eroot):
-    if not equiped[slot]:
-        equiped[slot] = new_weapon
-        weapon_inventory.remove(new_weapon)
+def new_equip(slot, current, iroot, eroot):
+    global attack
+    global defense
+
+    match slot:
+        case 0:
+            inv = weapon_inventory
+            func = get_weapon
+        case 1:
+            inv = helmet_inventory
+            func = get_helmet
+        case 2:
+            inv = chestpiece_inventory
+            func = get_chestpiece
+        case 3:
+            inv = leggings_inventory
+            func = get_leggings
+        case 4:
+            inv = boots_inventory
+            func = get_boots
     
+    if not equiped[slot]:
+        equiped[slot] = inv[current]
+        inv.remove(inv[current])
+
+        rows = func(equiped[slot])
+        for row in rows:
+            if slot > 0:
+                defense += row[1]
+            else:
+                attack += row[1]
+
+    else:
+        rows = func(equiped[slot])
+        for row in rows:
+            if slot > 0:
+                defense -= row[1]
+            else:
+                attack -= row[1]
+        
+        temp = equiped[slot]
+        equiped[slot] = inv[current]
+        inv[current] = temp
+
+        rows = func(equiped[slot])
+        for row in rows:
+            if slot > 0:
+                defense += row[1]
+            else:
+                attack += row[1]
+    
+    UnbindToolTip(character)
+    CreateToolTip(character, text = character_text)
+
     iroot.destroy()
     eroot.destroy()
     open_equipment()
 
+def remove_equip(slot, eroot):
+    global attack
+    global defense
 
+    match slot:
+        case 0:
+            inv = weapon_inventory
+            func = get_weapon
+        case 1:
+            inv = helmet_inventory
+            func = get_helmet
+        case 2:
+            inv = chestpiece_inventory
+            func = get_chestpiece
+        case 3:
+            inv = leggings_inventory
+            func = get_leggings
+        case 4:
+            inv = boots_inventory
+            func = get_boots
+
+    inv.append(equiped[slot])
+
+    rows = func(equiped[slot])
+    for row in rows:
+        if slot > 0:
+            defense += row[1]
+        else:
+            attack += row[1]
+    
+    equiped[slot] = ""
+
+    UnbindToolTip(character)
+    CreateToolTip(character, text = character_text)
+
+    eroot.destroy()
+    open_equipment()
+
+def sell_equip(slot, current, inv, a_root):
+    global gold
+    global attack
+    global defense
+    match slot:
+            case 0:
+                func = get_weapon
+            case 1:
+                func = get_helmet
+            case 2:
+                func = get_chestpiece
+            case 3:
+                func = get_leggings
+            case 4:
+                func = get_boots
+
+    if current < 0:
+        rows = func(equiped[slot])
+        for row in rows:
+            gold += row[2]
+            if slot > 0:
+                defense -= row[1]
+            else:
+                attack -= row[1]
+        
+        equiped[slot] = ""
+
+        a_root.destroy()
+        open_equipment()
+
+    else:
+        rows = func(inv[current])
+        for row in rows:
+            gold += row[2]
+
+        inv.remove(inv[current])
+        a_root.destroy()
+
+    UnbindToolTip(character)
+    CreateToolTip(character, text = character_text)
 
 
 def do_quit():
     with open('savefile.dat', 'wb') as f:
-        pickle.dump([level, experience, health, gold, equiped, weapon_inventory, helmet_inventory, chestpiece_inventory, leggings_inventory, boots_inventory], f)
+        pickle.dump([level, experience, health, gold, attack, defense, equiped, weapon_inventory, helmet_inventory, chestpiece_inventory, leggings_inventory, boots_inventory], f)
+
     quit()
 
 # Saved stats
 if (Path('savefile.dat').exists()):
     with open('savefile.dat', 'rb') as f:
-        level, experience, health, gold, equiped, weapon_inventor, helmet_inventory, chestpiece_inventory, leggings_inventory, boots_inventory = pickle.load(f)
+        level, experience, health, gold, attack, defense, equiped, weapon_inventory, helmet_inventory, chestpiece_inventory, leggings_inventory, boots_inventory = pickle.load(f)
 else:
     level = 1
     experience = 0
     health = 100
     gold = 0
+    attack = 1
+    defense = 1
     equiped = ["", "", "", "", ""]
-    weapon_inventory = ["Level 1 Common Sword"]
-    helmet_inventory = [""]
+    weapon_inventory = ["Common Level 1 Sword", "Uncommon Level 1 Sword"]
+    helmet_inventory = ["Legendary Level 1 Helmet"]
     chestpiece_inventory = [""]
     leggings_inventory = [""]
     boots_inventory = [""]
 
 
 # Default variables
+character_text = "Health: " + str(health) + "/100 \nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold)
 x = 2200
 y = 1280
 cycle = 0
@@ -159,9 +359,9 @@ event_number = random.randrange(1, 2, 1)
 root = tk.Tk()
 
 # Set gifs
-idle = [tk.PhotoImage(file='stand.gif', format = 'gif -index %i' %(i)) for i in range(2)]
-walk_positive = [tk.PhotoImage(file='left.gif', format = 'gif -index %i' %(i)) for i in range(2)]
-walk_negative = [tk.PhotoImage(file='right.gif', format = 'gif -index %i' %(i)) for i in range(2)]
+idle = [tk.PhotoImage(file='assets/character/stand.gif', format = 'gif -index %i' %(i)) for i in range(2)]
+walk_positive = [tk.PhotoImage(file='assets/character/left.gif', format = 'gif -index %i' %(i)) for i in range(2)]
+walk_negative = [tk.PhotoImage(file='assets/character/right.gif', format = 'gif -index %i' %(i)) for i in range(2)]
 
 
 # Play gif and potentially change event
@@ -191,10 +391,10 @@ def update(cycle, check, event_number, x):
 
     root.geometry('100x100+' + str(x) + '+' + str(y))
     character.configure(image = frame)
-    root.after(1, event, cycle, check, event_number, x)
+    root.after(1, set_event, cycle, check, event_number, x)
 
 # Set event
-def event(cycle, check, event_number, x):
+def set_event(cycle, check, event_number, x):
     if event_number in idle_num:
         check = 0
         print('idle')
@@ -220,12 +420,12 @@ root.wm_attributes('-topmost', '1')
 character = tk.Label(root, bd=0, bg='black')
 character.pack()
 
-CreateToolTip(character, text = "Health: " + str(health) + "/100 \nStrength: 1 \nDefense: 1")
+CreateToolTip(character, text = character_text)
 
 m = tk.Menu(root, tearoff = 0)
 m.add_command(label="Equipment", command=open_equipment)
 m.add_command(label="Exit", command=do_quit)
-character.bind("<Button-3>", do_menu)
+character.bind("<Button-3>", lambda event, menu = m: do_menu(event, menu))
 
 root.after(1, update, cycle, check, event_number, x)
 
