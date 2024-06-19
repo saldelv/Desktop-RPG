36bc5563f7933e9c1c0a30f572b030f8ea3aa282
Salvatore Del Vecchio <96501610+saldelv@users.noreply.github.com>
2024-06-19 14:39:08 -0400

    Added enemy spawning and battles

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import zlib
 import tempfile
 import pickle
 from pathlib import Path
+import time
 from database import *
 
 # Tooltip hover class
@@ -63,7 +64,6 @@ def open_equipment():
     ey = root.winfo_y() - 200
     eroot.geometry("+%d+%d" % (ex, ey))
     eroot.wm_attributes('-topmost', '1')
-
     
     weapon = get_equipped(0, eroot)
     headgear = get_equipped(1, eroot)
@@ -242,7 +242,7 @@ def new_equip(slot, current, iroot, eroot):
                 attack += row[1]
     
     UnbindToolTip(character)
-    CreateToolTip(character, text = "Health: " + str(health) + "/100 \nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold))
+    CreateToolTip(character, text = "Health: " + str(health) + "\nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold))
 
     iroot.destroy()
     eroot.destroy()
@@ -281,7 +281,7 @@ def remove_equip(slot, eroot):
     equiped[slot] = ""
 
     UnbindToolTip(character)
-    CreateToolTip(character, text = "Health: " + str(health) + "/100 \nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold))
+    CreateToolTip(character, text = "Health: " + str(health) + "\nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold))
 
     eroot.destroy()
     open_equipment()
@@ -325,7 +325,7 @@ def sell_equip(slot, current, inv, a_root):
         a_root.destroy()
 
     UnbindToolTip(character)
-    CreateToolTip(character, text = "Health: " + str(health) + "/100 \nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold))
+    CreateToolTip(character, text = "Health: " + str(health) + "\nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold))
 
 
 def do_quit():
@@ -351,6 +351,7 @@ else:
     chestpiece_inventory = ["Rare Level 1 Chestpiece"]
     leggings_inventory = [""]
     boots_inventory = [""]
+max_health = health
 
 
 # Default variables
@@ -362,6 +363,10 @@ idle_num=[1, 2, 3, 4]
 walk_left = [5, 6, 7]
 walk_right = [8, 9, 10]
 event_number = random.randrange(1, 2, 1)
+in_battle = False
+battle_distance = 0
+battle_moved = 0
+finished_battle = False
 
 root = tk.Tk()
 
@@ -382,6 +387,19 @@ def gif_work(cycle, frames, event_number, first_num, last_num):
 
 # Update current event
 def update(cycle, check, event_number, x):
+    global battle_moved, finished_battle
+    if in_battle or finished_battle:
+        if finished_battle:
+            movex = 15
+            if x >= 2200:
+                finished_battle = False
+        elif battle_moved < battle_distance:
+            movex = 15
+            battle_moved += movex
+        else:
+            movex = 0
+    else:
+        movex = 3
     if check == 0:
         frame = idle[cycle]
         cycle, event_number = gif_work(cycle, idle, event_number, 1, 9)
@@ -389,33 +407,126 @@ def update(cycle, check, event_number, x):
     elif check == 1:
         frame = walk_positive[cycle]
         cycle, event_number = gif_work(cycle, walk_positive, event_number, 1, 9)
-        x -= 3
+        x -= movex
 
     elif check == 2:
         frame = walk_negative[cycle]
         cycle, event_number = gif_work(cycle, walk_negative, event_number, 1, 9)
-        x += 3
+        x += movex 
 
     root.geometry('100x100+' + str(x) + '+' + str(y))
     character.configure(image = frame)
-    root.after(1, set_event, cycle, check, event_number, x)
+    if in_battle:
+        if battle_moved < battle_distance:
+            root.after(100, update, cycle, 1, event_number, x)
+        else:
+            root.after(400, update, cycle, 0, event_number, x)
+    elif finished_battle:
+        root.after(100, update, cycle, 2, event_number, x)
+    else:
+        root.after(1, set_event, cycle, check, event_number, x)
 
 # Set event
 def set_event(cycle, check, event_number, x):
-    if event_number in idle_num:
-        check = 0
-        print('idle')
-        root.after(400, update, cycle, check, event_number, x)
+    if not in_battle:
+        if event_number in idle_num:
+            check = 0
+            print('idle')
+            root.after(400, update, cycle, check, event_number, x)
 
-    elif event_number in walk_left:
-        check = 1
-        print('walk left')
-        root.after(100, update, cycle, check, event_number, x)
+        elif event_number in walk_left:
+            check = 1
+            print('walk left')
+            root.after(100, update, cycle, check, event_number, x)
 
-    elif event_number in walk_right:
-        check = 2
-        print('walk right')
-        root.after(100, update, cycle, check, event_number, x)
+        elif event_number in walk_right:
+            check = 2
+            print('walk right')
+            root.after(100, update, cycle, check, event_number, x)
+
+def spawn_enemy():
+    timer = 0
+
+    enemy = tk.Label(enemy_root, bd=0, bg='black')
+
+    enemy.place(x = random.randrange(0, 400), y = -50)
+    root.after(1, play_enemy(enemy, 0))
+
+    enemy.bind("<Button-1>", lambda event: start_battle(enemy))
+
+    enemy.health = 100
+    enemy.max_health = 100
+    enemy.attack = 1
+    enemy.defense = 1
+    enemy.experience = 10
+    CreateToolTip(enemy, text = "Slime" + "\nHealth: " + str(enemy.health))
+
+    root.after(60000, lambda: despawn_enemy(enemy))
+    root.after(30000, spawn_enemy)
+
+def despawn_enemy(enemy):
+    enemy.destroy()
+
+def play_enemy(enemy, current_frame):
+    img = [tk.PhotoImage(file='assets/enemies/slime.gif', format = 'gif -index %i' %(i)) for i in range(6)]
+    frame = img[current_frame]
+    enemy.configure(image = frame)
+    enemy.image = frame
+    current_frame = current_frame + 1
+    if current_frame == len(img):
+        current_frame = 0
+    if not finished_battle:
+        root.after(200, lambda: play_enemy(enemy, current_frame))
+    else:
+        despawn_enemy(enemy)
+
+def start_battle(enemy):
+    global in_battle, battle_distance
+    if not in_battle:
+        in_battle = True
+        battle_distance = (x - 1850) + (310-enemy.winfo_x())
+    
+    if battle_moved >= battle_distance:
+        enemy_health = tk.Label(enemy_root, text = "Enemy:\n" + str(enemy.health) + "/" + str(enemy.max_health))
+        enemy_health.place(x = enemy.winfo_x() + 75, y = enemy.winfo_y() - 90)
+        enemy_health.grid(row = 0, column = 0)
+        player_health = tk.Label(enemy_root, text = "Player:\n" + str(health) + "/" + str(max_health))
+        player_health.place(x = enemy.winfo_x() + 50, y = enemy.winfo_y() - 50)
+        player_health.grid(row = 0, column = 1)
+        root.after(400, lambda: update_battle(enemy, enemy_health, player_health))
+    else:
+        root.after(100, lambda: start_battle(enemy))
+
+def update_battle(enemy, enemy_health, player_health):
+    global health, experience, in_battle, finished_battle, battle_distance, battle_moved
+    enemy.health -= max(attack - enemy.defense, 0)
+    health -= max(enemy.attack - defense, 0)
+    enemy_health.configure(text = "Enemy:\n" + str(max(enemy.health, 0)) + "/" + str(enemy.max_health))
+    player_health.configure(text = "Player:\n" + str(max(health, 0)) + "/" + str(max_health))
+
+    if enemy.health <= 0:
+        print("win")
+        experience += enemy.experience
+        health = max_health
+        in_battle = False
+        finished_battle = True
+        battle_distance = 0
+        battle_moved = 0
+        player_health.destroy()
+        enemy_health.destroy()
+    elif health <= 0:
+        print("lose")
+        health = max_health
+        in_battle = False
+        finished_battle = True
+        battle_distance = 0
+        battle_moved = 0
+        player_health.destroy()
+        enemy_health.destroy()
+    else:
+        print("next turn")
+        root.after(500, lambda: update_battle(enemy, enemy_health, player_health))
+    
 
 # Make background transparent and always on top
 root.config(highlightbackground='black')
@@ -427,13 +538,24 @@ root.wm_attributes('-topmost', '1')
 character = tk.Label(root, bd=0, bg='black')
 character.pack()
 
-CreateToolTip(character, text = "Health: " + str(health) + "/100 \nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold))
+CreateToolTip(character, text = "Health: " + str(health) + "\nAttack: " + str(attack) + "\nDefense: " + str(defense) + "\nGold: " + str(gold))
 
 m = tk.Menu(root, tearoff = 0)
 m.add_command(label="Equipment", command=open_equipment)
 m.add_command(label="Exit", command=do_quit)
 character.bind("<Button-3>", lambda event, menu = m: do_menu(event, menu))
 
+enemy_root = tk.Toplevel(root)
+enemy_root.config(highlightbackground='black')
+enemy_root.overrideredirect(True)
+enemy_root.wm_attributes('-transparentcolor', 'black')
+enemy_root['bg'] = 'black'
+enemy_root.wm_attributes('-topmost', '1')
+
+enemy_root.geometry("+%d+%d" % (x - 800, y))
+enemy_root.geometry('500x500')
+
 root.after(1, update, cycle, check, event_number, x)
+enemy_root.after(3000, spawn_enemy)
 
 root.mainloop()
